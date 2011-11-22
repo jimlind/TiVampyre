7043bf8ece8d6518a22a7839d24bb757b73d34ef
Jim Lind <spoon.vw@gmail.com>
2011-11-21 22:28:12 -0600

    Complete Transition to HandBrakeCLI. Single Pass Encoding with Quality based on final width of video. Chop commercials after encoded to MP4 because it's the only way that works great.

--- application/libraries/Video.php
+++ application/libraries/Video.php
@@ -7,12 +7,13 @@ class Video {
         $resize = $job->full != 1;
 	$crop = $job->crop == 1;
 	$size = $this->getSize($input);
-	
-        $bits = $this->getVideoBitrate($size['height'], $size['width']);
-        $this->encode($input, $bits, $resize, $crop, $output);
-	if ($job->chop == 1) {
-            $this->chopCommercials($input, $output);
-        }
+	if ($resize && $size['width'] > 1024) {
+	    $size['height'] = 1024/($size['width']/$size['height']);
+	    $size['width'] = 1024;
+	}
+        $quality = $this->getVideoQuality($size['height'], $size['width']);
+        $this->encode($input, $quality, $resize, $crop, $output);
+	$this->fix($output, $job->chop, $input);
 	
         return file_exists($output);
     }
@@ -33,18 +34,23 @@ class Video {
 	return array('width'=>$matches[1][0], 'height'=>$matches[1][1]);
     }
     
-    private function getVideoBitrate($height, $width)
+    private function getVideoQuality($height, $width)
     {
-        $square = $height * $width;
-        $bitrate = (0.0013 * $square) + 410;
-        return round($bitrate);
+	// computers don't care if this is left unsimplified
+	// it'll make it easier to edit later if neccesary.
+	$w1 = 704;	//width
+	$q1 = 23;	//quality
+	$w2 = 1920;	//width
+	$q2 = 28;	//quality
+	
+	// just using a linear equation
+	$qOut = (($q2 - $q1) / ($w2 - $w1) * ($width - $w1)) + $q1;
+        return round($qOut);
     }
  
-     private function encode($target, $bits, $resize, $crop, $output)
+     private function encode($target, $quality, $resize, $crop, $output)
     {
         $addFilter = "";
-        $height = $size['height'];
-        $width = $size['width'];
 	
 	$ci =& get_instance();
 	$vConfig = $ci->config->item('tivampyre');
@@ -56,15 +62,15 @@ class Video {
 	$h .= "-o $output "; //output
 	
 	// video encoding
-	$h .= "-e x264 -x ref=2:bframes=0 "; //codec and settings
-	$h .= "-b $bits ";	//bitrate
+	$h .= "-e x264 -x b-adapt=2:rc-lookahead=50 "; //Normal Encoding Preset
+	$h .= "-q $quality ";	//constant quality
 	$h .= "-r 29.97 ";	//framerate
 	
 	// audio encoding
 	$h .= "-E faac ";	//codec
-	$h .= "-B 128 ";	//bitrate
+	$h .= "-B 320 ";	//bitrate
 	$h .= "-6 stereo ";	//down sample to stereo
-	$h .= "-D 1.5 ";	//dynamic volume compression
+	$h .= "-D 1.0 ";	//dynamic volume compression
 
 	// crop and resize
 	if ($resize) {
@@ -74,12 +80,9 @@ class Video {
 	    $h .= "--crop 0:0:0:0 ";	//no cropping, default is auto crop
 	}
 	
-	// filters
-	$h .= "-d slower -5 ";		// deinterlace and decomb
+	// filters		
+	$h .= "--detelecine --decomb ";	// detelecine and decomb
 	$h .= "--loose-anamorphic ";	// keep a good aspect ratio
-
-	// enable two pass encoding
-	$h .= "-2 -T ";
 
 	log_message('debug', $h);
         shell_exec($h);
@@ -87,7 +90,54 @@ class Video {
         return file_exists($output);
     }
     
-    private function chopCommercials($mpeg, $mp4)
+    private function fix($mp4, $chop, $mpeg)
+    {
+	$fileRoot = substr($mp4, 0, strrpos($mp4, '.'));
+	$clean = $fileRoot . ".clean.mp4";
+	
+	if ($chop == 1) {
+	    $this->chopCommercials($mp4, $mpeg, $fileRoot, $clean);
+	} else {
+	    $m  = "mencoder $mp4 ";	// input
+	    $m .= "-oac faac -faacopts mpeg=4:object=2:raw:br=128 "; //encode audio
+	    $m .= "-af volnorm=1 ";	// normalize audio volume
+	    $m .= "-ovc copy ";		// copy video
+	    $m .= "-of lavf ";		// force standard output
+	    $m .= "-o $clean";		// output
+	    
+	    log_message('debug', $m);
+	    shell_exec($m);
+	}
+	
+	$cleanT1 = $fileRoot . ".clean_track1.h264";
+	$cleanT2 = $fileRoot . ".clean_track2.aac";
+	$remux = $fileRoot . ".remux.mp4";
+	
+	$t1 = "MP4Box -raw 1 $clean";
+	log_message('debug', $t1);
+	shell_exec($t1);	
+
+	$t2 = "MP4Box -raw 2 $clean";
+	log_message('debug', $t2);
+	shell_exec($t2);
+
+	$r = "MP4Box -new $remux -add $cleanT1 -add $cleanT2";
+	log_message('debug', $r);
+	shell_exec($r);
+
+	// Remove or rename files
+        try {
+	    unlink($clean);
+	    unlink($cleanT1);
+	    unlink($cleanT2);
+	    unlink($mp4);
+	    rename($remux, $mp4);
+	} catch(Exception $e) {
+            //do nothing
+        }
+    }
+    
+    private function chopCommercials($mp4, $mpeg, $root, $clean)
     {
 	$ci =& get_instance();
 	$vConfig = $ci->config->item('tivampyre');
@@ -101,17 +151,12 @@ class Video {
 	log_message('debug', $c);
 	shell_exec($c);
 	
-        $pos = strrpos($mp4, '.');
-        $fileRoot = substr($mp4, 0, $pos);
-        $edl = $fileRoot . ".edl";
-	$clean = $fileRoot . ".clean.mp4";
-	$cleanT1 = $fileRoot . ".clean_track1.h264";
-	$cleanT2 = $fileRoot . ".clean_track2.aac";
-	$remux = $fileRoot . ".remux.mp4";
+        $edl = $root . ".edl";
 	
-	$m  = "mencoder $mp4 ";	// input
+	$m  = "mencoder $mp4 ";		// input
 	$m .= "-edl $edl ";		// use commercials file
 	$m .= "-oac faac -faacopts mpeg=4:object=2:raw:br=128 "; //encode audio
+	$m .= "-af volnorm=1 ";		// normalize audio volume
 	$m .= "-ovc copy ";		// copy video
 	$m .= "-of lavf ";		// force standard output
 	$m .= "-o $clean";		// output
@@ -119,29 +164,12 @@ class Video {
 	log_message('debug', $m);
 	shell_exec($m);
 
-	$t1 = "MP4Box -raw 1 $clean";
-	log_message('debug', $t1);
-	shell_exec($t1);	
-
-	$t2 = "MP4Box -raw 2 $clean";
-	log_message('debug', $t2);
-	shell_exec($t2);
-
-	$r = "MP4Box -new $remux -add $cleanT1 -add $cleanT2";
-	log_message('debug', $r);
-	shell_exec($r);	
-
 	// Remove all the extra files hanging around
         try {
             unlink($edl);
-	    unlink($fileRoot . ".log");
-            unlink($fileRoot . ".logo.txt");
-            unlink($fileRoot . ".txt");
-	    unlink($clean);
-	    unlink($cleanT1);
-	    unlink($cleanT2);
-	    unlink($mp4);
-	    rename($remux, $mp4);
+	    unlink($root . ".log");
+            unlink($root . ".logo.txt");
+            unlink($root . ".txt");
         } catch(Exception $e) {
             //do nothing
         }
