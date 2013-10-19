c4114a0afce521efe92b180455ac4efb549b68db
Jim Lind <spoon.vw@gmail.com>
2013-10-19 18:24:11 -0500

    Some JavaScript.
    Just a little something to click on.
    No JS framework needed.

--- src/controllers.php
+++ src/controllers.php
@@ -6,21 +6,22 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-$app->get('/', function () use ($app) {
+$app->get('/', function() use ($app) {
 	$showResults = $app['db']->fetchAll('SELECT * FROM shows ORDER BY show_title, episode_number, date');
     return $app['twig']->render('index.html.twig', array(
 		'results' => $showResults,
 	));
-})
-->bind('homepage')
-;
-
+});
+$app->post('/submit', function(Request $request) use ($app) {
+	$id = $request->get('id');
+	$data = array('apple', 'orange', $id);
+	return new JsonResponse($data);
+});
 $app->error(function (\Exception $e, $code) use ($app) {
-    if ($app['debug']) {
-        return;
-    }
-
-    $page = 404 == $code ? '404.html.twig' : '500.html.twig';
-
+    if (in_array($code, array(404, 405))) {
+		$page = '404.html.twig';
+	} else {
+		$page = '500.html.twig';
+	}
     return new Response($app['twig']->render($page, array('code' => $code)), $code);
 });
