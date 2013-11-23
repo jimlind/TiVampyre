676613b3a6034cf4cc2379fcb88f3a28fb8dee44
Jim Lind <jimlind@me.com>
2013-11-23 00:21:34 -0600

    Unit tests for Google
    White spacing on google image service as well.

--- src/JimLind/Image/Google.php
+++ src/JimLind/Image/Google.php
@@ -6,27 +6,27 @@ use Symfony\Component\Process\Process;
 
 class Google {
 
-	private $key;
-	private $process;
+    private $key;
+    private $process;
 
-	function __construct($key, Process $process) {
-		$this->key = $key;
-		$this->process = $process;
-	}
+    function __construct($key, Process $process) {
+        $this->key = $key;
+        $this->process = $process;
+    }
 
-	function getOneURL($keywords) {
-		$url = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=';
-		$url .= urlencode($keywords).'&start=0&key='.$this->key;
-		$url .= '&as_filetype=jpg&imgsz=medium|large';
+    function getOneURL($keywords) {
+        $url = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=';
+        $url .= urlencode($keywords).'&start=0&key='.$this->key;
+        $url .= '&as_filetype=jpg&imgsz=medium|large';
 
-		$command = "curl -s '$url'";
+        $command = "curl -s '$url'";
 
-		$this->process->setCommandLine($command);
-		$this->process->setTimeout(30); // 30 seconds
-		$this->process->run();
+        $this->process->setCommandLine($command);
+        $this->process->setTimeout(30); // 30 seconds
+        $this->process->run();
 
-		$output = $this->process->getOutput();
-		$object = json_decode($output);
-		return $object->responseData->results[0]->unescapedUrl;
-	}
+        $output = $this->process->getOutput();
+        $object = json_decode($output);
+        return $object->responseData->results[0]->unescapedUrl;
+    }
 }
---
+++ tests/JimLind/Image/GoogleTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace JimLind\Image;
+
+class GoogleTest extends \PHPUnit_Framework_TestCase {
+
+    private $key;
+    private $process;
+    private $fixture;
+
+    public function setUp() {
+        $this->key = (string) rand();
+        
+        $mockProcessBuilder = $this->getMockBuilder('\Symfony\Component\Process\Process');
+	$this->process = $mockProcessBuilder->disableOriginalConstructor()->getMock();
+        
+        $this->fixture = new \JimLind\Image\Google($this->key, $this->process);
+    }
+
+    /**
+     * @dataProvider getOneURLProvider
+     */
+    public function testGetOneURL($keywords, $expected) {
+        $this->expected = $expected;
+        
+        $this->process->expects($this->once())
+            ->method('setCommandLine')
+            ->with($this->callback(function($input) {
+                return (strpos($input, $this->expected) !== false);
+            }));
+        $this->process->expects($this->once())
+            ->method('setTimeout');
+        $this->process->expects($this->once())
+            ->method('run');
+        $this->process->expects($this->once())
+            ->method('getOutput')
+            ->will($this->returnValue('{"responseData":{"results":[{"unescapedUrl":"href"}]}}'));
+        
+        $this->fixture->getOneURL($keywords);
+    }
+
+    public function getOneURLProvider() {
+        return array(
+            array(
+                'keywords' => 'foo',
+                'expects' => 'q=foo&start=0&key=' . $this->key
+            ),
+            array(
+                'keywords' => 'bar',
+                'expects' => 'q=bar&start=0&key=' . $this->key
+            ),
+            array(
+                'keywords' => 'foo bar',
+                'expects' => 'q=foo+bar&start=0&key=' . $this->key
+            ),
+        );
+    }
+
+}
