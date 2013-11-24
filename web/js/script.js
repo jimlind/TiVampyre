189648d92bcd61f0659df2cd7d834fe6ce328f26
Jim Lind <jimlind@me.com>
2013-11-23 21:32:28 -0600

    Front End Updates
    * Remove header text.
    * Add a dynamic column calculation.
    * Whitespace in JS updates.

--- web/js/script.js
+++ web/js/script.js
@@ -1,43 +1,57 @@
 // Wait for the DOM to be ready.
 document.addEventListener('DOMContentLoaded', function () {
 
-        // Collect shows.
-        var shows = document.getElementsByClassName('show');
-        for (var i = 0; i < shows.length; ++i) {
-            var show = shows[i];
-            // Collect show image placeholders and load.
-            var logos = show.getElementsByClassName('showLogo');
-            if (logos.length === 1) {
-                loadImage(logos[0]);
-            }
-            // Collect episodes count and display.
-            var episodes = show.getElementsByClassName('episode').length;
-            var showingText = episodes + " showing";
-            if (episodes > 1) {
-                showingText += "s";
-            }
-            // Fill showing count text.
-            var countSpans = show.getElementsByClassName('showingCount')
-            if (countSpans.length === 1) {
-                countSpans[0].innerHTML = showingText;
+    // Collect shows.
+    var shows = document.getElementsByClassName('show');
+    for (var i = 0; i < shows.length; ++i) {
+        var show = shows[i];
+        // Collect show image placeholders and load.
+        var logos = show.getElementsByClassName('showLogo');
+        if (logos.length === 1) {
+            loadImage(logos[0]);
+        }
+        // Collect episodes count and display.
+        var episodes = show.getElementsByClassName('episode').length;
+        var showingText = episodes + " showing";
+        if (episodes > 1) {
+            showingText += "s";
+        }
+        // Fill showing count text.
+        var countSpans = show.getElementsByClassName('showingCount')
+        if (countSpans.length === 1) {
+            countSpans[0].innerHTML = showingText;
+        }
+    }
+    
+    // Dynamically resize the show width.
+    window.onresize = function(){
+        columns = Math.min(Math.floor(window.innerWidth / 205), 9);
+        prevColStyle = shows[0].getAttribute('data-cols');
+        // If the columns change.
+        if (columns !== prevColStyle) {
+            // Apply to all shows.
+            for (var i = 0; i < shows.length; ++i) {
+                show = shows[i];
+                show.classList.remove('cols' + prevColStyle);;
+                show.classList.add('cols' + columns);
+                show.setAttribute('data-cols', columns);
             }
         }
+    };
+    window.dispatchEvent(new Event('resize'));
         
-	// Collect clickable shows and loop over them.
-	var shows = document.getElementsByClassName('clickableShow');
-	for (var i = 0; i < shows.length; ++i) {
-		// Perform action when clicked.
-		shows[i].onclick = function(x){
-			var id = this.getAttribute("data-id");
-			var xhr = new XMLHttpRequest();
-			xhr.open('POST', '/submit', true);
-			xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-			xhr.onload = function() {
-				console.log(this.responseText);
-			}
-			xhr.send('id='+id);
-		};
-	}
+    // Collect clickable shows and loop over them.
+    var episodes = document.getElementsByClassName('clickableEpisode');
+    for (var i = 0; i < episodes.length; ++i) {
+        // Perform action when clicked.
+        episodes[i].onclick = function(x){
+            var id = this.getAttribute("data-id");
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/submit', true);
+            xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            xhr.send('id='+id);
+        };
+    }
 });
 
 function loadImage(img) {
