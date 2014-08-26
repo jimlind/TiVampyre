43e9af8c099f76eb0248d8ba37b338cd3b88c951
Jim Lind <jimlind@me.com>
2014-08-25 23:08:33 -0500

    The simple no cache version of ShowImage

---
+++ web/js/ShowImage.js
@@ -0,0 +1,42 @@
+ShowImage = function(){}
+
+ShowImage.prototype.addShowImage = function(image)
+{
+    this.loadImageRemote(image);
+};
+
+ShowImage.prototype.loadImageRemote = function(image)
+{
+    var self = this;
+    var title = image.getAttribute("data-title");
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/image', true);
+    xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xhr.onload = function() {
+        var response = JSON.parse(this.responseText);
+        var base64 = response['base64'];
+        self.setImage(image, base64);
+        self.saveImage(title, base64);
+    };
+    xhr.send('title=' + title);
+};
+
+ShowImage.prototype.saveImage = function(title, base64)
+{
+    return 0;
+}
+
+ShowImage.prototype.setImage = function(image, base64)
+{
+    image.src="data:image/png;base64," + base64;
+};
+
+ShowImage.prototype.getHash = function(inputString)
+{
+    var inputArray = inputString.split("");
+    var hashed = inputArray.reduce(function(a, b) {
+        a = ((a << 5) - a) + b.charCodeAt(0);
+        return a & a;
+    }, 0);
+    return hashed;
+};
