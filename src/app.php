d1295da7a91619b0ae4f0d3c736818be10e0f737
Jim Lind <jimlind@me.com>
2014-09-17 22:28:05 -0500

    Remove Debugging bits and Monolog Service Provider
    Monolog Service Provider threw recursive JSON warnings.
    Just a warning, but still not something I want.

--- src/app.php
+++ src/app.php
@@ -2,22 +2,26 @@
 
 use Dflydev\Silex\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
 use Igorw\Silex\ConfigServiceProvider;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 use Silex\Application;
 use Silex\Provider\DoctrineServiceProvider;
-use Silex\Provider\MonologServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Symfony\Component\Process\Process;
 
+use JimLind\TiVo;
+
 // Set TimeZone
 date_default_timezone_set('America/Chicago');
 
-$configFile = __DIR__ . '/../config/tivampyre.jsonX';
+// Check that config exists
+$configFile = __DIR__ . '/../config/tivampyre.json';
 if (file_exists($configFile) == false) {
     throw new Exception('No Config File. Fix that.');
 }
-
+        
 $app = new Application();
 $app->register(new ConfigServiceProvider($configFile));
 
@@ -26,6 +30,7 @@ if (!isset($app['tivo_ip'])) {
     $app['tivo_ip'] = false;
 }
 
+// Start Registering Services
 $app->register(new DoctrineServiceProvider(), array(
     'db.options' => array(
         'driver' => 'pdo_sqlite',
@@ -44,16 +49,18 @@ $app->register(new DoctrineOrmServiceProvider, array(
         ),
     ),
 ));
-$app->register(new MonologServiceProvider(), array(
-    'monolog.logfile' => __DIR__.'/../logs/tivampyre.log',
-    'monolog.level'   => \Monolog\Logger::WARNING,
-));
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new TwigServiceProvider(), array(
     'twig.path'    => array(__DIR__.'/../templates'),
     //'twig.options' => array('cache' => __DIR__.'/../cache/twig'),
 ));
+
+// Setup Monolog Logger
+$logHandler = new StreamHandler(__DIR__.'/../logs/tivampyre.log');
+$app['monolog'] = new Logger('tivampyre');
+$app['monolog']->pushHandler($logHandler);
+
 $app['process'] = new Process('');
 $app['twitter'] = new Twitter(
     $app['twitter_consumer_key'],
@@ -67,9 +74,8 @@ $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
 }));
 $app['tivo_locater'] = function ($app) {
     return new TiVo\Location(
-        $app['monolog'],
-        $app['process'],
-        $app['tivo_ip']
+        new Process(''),
+        $app['monolog']
     );
 };
 $app['tivo_now_playing'] = function ($app) {
