aadf97988366630a7a5e39dfb31cf94c2baedda1
Jim Lind <jimlind@me.com>
2014-11-22 19:11:56 -0600

    Move some old junk into the Service folder.
    Probably some of this could be needed for refactoring.

--- src/TiVampyre/Service/ShowBlob.php
+++ src/TiVampyre/Service/ShowBlob.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * TiVampyre Show Service
- * 
- * PHP version 5
- * 
- * @category Service
- * @package  TiVampyre
- * @author   Jim Lind <spoon.vw@gmail.com>
- * @license  The MIT License
- * @link     https://github.com/jimlind/TiVampyre
- */
 
 namespace TiVampyre\Service;
 
@@ -23,16 +12,10 @@ use Twitter;
  * This service contains all the heavy lifting involved with importing shows.
  * It will get XML data from the NowPlaying Class put it in the appropiate
  * places, and generate and sent Tweets as neccessary.
- * 
- * @category Service
- * @package  TiVampyre
- * @author   Jim Lind <spoon.vw@gmail.com>
- * @license  The MIT License
- * @link     https://github.com/jimlind/TiVampyre
  */
-class Show
+class ShowBlob
 {
-    
+
     private $_entityManager;
     private $_nowPlaying;
     private $_twitter;
@@ -40,10 +23,10 @@ class Show
     private $_repository;
     private $_timestamp;
     private $_tweets;
-       
+
     /**
      * Constructor.
-     * 
+     *
      * @param EntityManager $entityManager Doctrine Entity Manager
      * @param NowPlaying    $nowPlaying    Access to Now Playing list
      * @param Twitter       $twitter       Twitter API translator
@@ -62,10 +45,10 @@ class Show
         $this->_repository = $entityManager->getRepository("TiVampyre\Entity\Show");
         $this->_tweets = array();
     }
-    
+
     /**
      * Gets extremely basic show information from the show repository.
-     * 
+     *
      * @return array Title and Quantity from database.
      */
     public function getHomepageData()
@@ -79,22 +62,22 @@ class Show
         $query = $this->_entityManager->createQuery($dql);
         return $query->getResult();
     }
-    
+
     /**
      * Gets all episode entities based on a show name
-     * 
+     *
      * @param string $name
-     * 
+     *
      * @return array An array of entities.
      */
     public function getEpisodes($name)
     {
         return $this->_repository->findByShowTitle($name);
     }
-    
+
     /**
      * Downloads all available shows and send them to be processed.
-     * 
+     *
      * @return null No return
      */
     public function rebuildLocalIndex()
@@ -102,7 +85,7 @@ class Show
         $allAvailableShows = $this->_nowPlaying->download();
         $isInitialRun = ($this->_repository->countAll() == 0);
         $this->_timestamp = new \DateTime('now');
-        // Word on the street is array_walk is faster than foreach.        
+        // Word on the street is array_walk is faster than foreach.
         array_walk(
             $allAvailableShows,
             array($this, '_processShow'),
@@ -110,18 +93,18 @@ class Show
         );
         $this->_entityManager->flush();
     }
-    
+
     /**
      * Writes a show entity to the database and compose a Tweet if neccessary.
-     * 
+     *
      * @param SimpleXMLElement $showXML   Show data in XML format
      * @param integer          $index     Ignore value from array walk
      * @param boolean          $dontTweet Don't compose a Tweet
-     * 
+     *
      * @return null                       No return
      */
     private function _processShow($showXML, $index, $dontTweet = false)
-    {        
+    {
         $show = new Entity\Show();
         $show->setTimeStamp($this->_timestamp);
         $show->populate($showXML);
@@ -133,12 +116,12 @@ class Show
         // Merge instead of persist because we don't have original data object.
         $this->_entityManager->merge($show);
     }
-    
+
     /**
      * Using an entity write a Tweet and add it to an array for later usage.
-     * 
+     *
      * @param Entity/Show $show A show entity
-     * 
+     *
      * @return null             No return
      */
     private function _composeTweet($show)
@@ -153,26 +136,26 @@ class Show
             $tweet .= ' in HD';
         }
         $this->_tweets[] = $tweet . '.';
-    } 
-    
+    }
+
     /**
      * Walk through the tweets array calling a function for each.
-     * 
+     *
      * @return null No return
      */
     public function sendTweets()
-    {        
+    {
         array_walk(
             $this->_tweets,
             array($this, '_sendTweet')
         );
     }
-    
+
     /**
      * Send a Tweet or log a warning.
-     * 
+     *
      * @param string $tweet A message indicating that the show started recording.
-     * 
+     *
      * @return null No return
      */
     private function _sendTweet($tweet)
@@ -183,5 +166,5 @@ class Show
             $this->_logger->addWarning($tweet);
             $this->_logger->addWarning($e->getMessage());
         }
-    }   
+    }
 }
