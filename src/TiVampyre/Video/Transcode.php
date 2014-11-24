8e9edc95b15c48a34fd427590f9fd19c3bf97e62
Jim Lind <jimlind@me.com>
2014-11-23 18:11:20 -0600

    Allow Autocropping
    Also, drop a lot of unused code.

--- src/TiVampyre/Video/Transcode.php
+++ src/TiVampyre/Video/Transcode.php
@@ -28,7 +28,7 @@ class Transcode
         $this->logger  = $logger;
     }
 
-    public function transcode($input, $output)
+    public function transcode($input, $output, $autocrop = false)
     {
         if (!Utilities::checkHandBrake($this->process)) {
             $warning = 'The HandBrake tool can not be trusted or found. ';
@@ -39,12 +39,15 @@ class Transcode
 
         $videoInfo  = new Info($input, $this->process, $this->logger);
         $resolution = $videoInfo->getIdealResolution($input);
-        $crop       = $videoInfo->getCropValues();
         $quality    = $this->getVideoQuality($resolution['height'], $resolution['width']);
 
-        //var_dump($resolution, $quality, $crop);
-        //die;
-        $this->encode($input, $output, $resolution, $quality);
+        if ($autocrop) {
+            $crop = $videoInfo->getCropValues();
+        } else {
+            $crop = false;
+        }
+
+        $this->encode($input, $output, $resolution, $crop, $quality);
     }
 
     protected function getVideoQuality($height, $width)
@@ -59,7 +62,7 @@ class Transcode
         return round($qOut);
     }
 
-    protected function encode($input, $output, $resolution, $quality)
+    protected function encode($input, $output, $resolution, $crop, $quality)
     {
 	$command  = 'HandBrakeCLI -i ' . $input . ' -o ' . $output;
 
@@ -74,121 +77,17 @@ class Transcode
 	// Resize and Crop
 	$command .= ' -w ' . $resolution['width'];
         $command .= ' -l ' . $resolution['height'];
-        //$h .= " --crop {$crop['top']}:{$crop['bottom']}:{$crop['left']}:{$crop['right']} ";
+        if ($crop) {
+            $command .= ' --crop ' . implode(':', $crop);
+        } else {
+            $command .= ' --crop 0:0:0:0';
+        }
 
 	// Filters
-	$command .= "--detelecine --decomb "; // Detelecine and Decomb
+	$command .= " --detelecine --decomb "; // Detelecine and Decomb
 
         $this->process->setCommandLine($command);
         $this->process->setTimeout(0); // Don't timeout.
         $this->process->run();
     }
-
-    protected function getOutputDimensions($target, $crop, $resize)
-    {
-        $s = array(
-            'width' => 0,
-            'height'  => 0,
-        );
-
-        $c = array(
-            'top'    => 0,
-            'bottom' => 0,
-            'left'   => 0,
-            'right'  => 0,
-        );
-
-        // start command line
-        $h  = "HandBrakeCLI ";
-        $h .= "-i $target "; //input
-        $h .= "-o /dev/null "; //output
-
-        $h .= "--start-at duration:0 ";
-        $h .= "--stop-at duration:1 ";
-
-        $h .= "2>&1";
-
-        log_message('debug', $h);
-        $o = shell_exec($h);
-
-        $pattern = '|\+ size: (\d+)x(\d+),|';
-        preg_match_all($pattern, $o, $matches);
-
-        $s['width']  = intval($matches[1][0]);
-        $s['height'] = intval($matches[2][0]);
-
-        // Find aspect ratio
-        $pattern = '|, aspect ([\d.]+):([\d.]+),|';
-        preg_match_all($pattern, $o, $matches);
-        $aspectRatio = $matches[1][0] / $matches[2][0];
-
-
-        // Adjust height or width if neccesary to match aspect ratio (scale up)
-        $ourRatio = $s['width'] / $s['height'];
-        $range = 0.1;
-        if (($ourRatio < $aspectRatio - $range) || ($ourRatio > $aspectRatio + $range)) {
-            // Check adding height
-            $newHeight = $s['width'] / $aspectRatio;
-            $newHeightArea = $s['width'] * $newHeight;
-            // Check adding width
-            $newWidth = $s['height'] * $aspectRatio;
-            $newWidthArea = $s['height'] * $newWidth;
-            // Apply best fit
-            if ($newHeightArea > $newWidthArea) {
-                $s['height'] = $newHeight;
-            } else {
-                $s['width'] = $newWidth;
-            }
-        }
-
-        if ($crop) {
-            $pattern = '|autocrop = (\d+)/(\d+)/(\d+)/(\d+)|';
-            preg_match_all($pattern, $o, $matches);
-
-            $c['top']    = intval($matches[1][0]);
-            $c['bottom'] = intval($matches[2][0]);
-            $c['left']   = intval($matches[3][0]);
-            $c['right']  = intval($matches[4][0]);
-        }
-
-        $outSize = array();
-        $outSize['height'] = $s['height'] - $c['top'] - $c['bottom'];
-        $outSize['width']  = $s['width'] - $c['left'] - $c['right'];
-
-        // Default HD content to 1024 wide for iPads
-        if ($resize && $outSize['width'] > 1024) {
-	    $outSize['height'] = 1024/($outSize['width']/$outSize['height']);
-	    $outSize['width'] = 1024;
-	}
-
-        // Try to match standard HD sizes
-        $range = 24;
-        $h = 1080;
-        $w = 1920;
-        if (($outSize['height'] >= $h - $range) && ($outSize['height'] <= $h + $range) &&
-            ($outSize['width'] >= $w - $range) && ($outSize['width'] <= $w + $range)) {
-            log_message('debug', "Force 1080p Resolution");
-	    $outSize['height'] = $h;
-            $outSize['width'] = $w;
-        }
-        $range = 12;
-        $h = 720;
-        $w = 1280;
-        if (($outSize['height'] >= $h - $range) && ($outSize['height'] <= $h + $range) &&
-            ($outSize['width'] >= $w - $range) && ($outSize['width'] <= $w + $range)) {
-            log_message('debug', "Force 720p Resolution");
-	    $outSize['height'] = $h;
-            $outSize['width'] = $w;
-        }
-
-        // Whole Integers
-        $outSize['height'] = intval($outSize['height']);
-        $outSize['width'] = intval($outSize['width']);
-
-        return array(
-            'size' => $outSize,
-            'crop' => $c,
-        );
-    }
-
 }
