f9869cb25a24c19cd9a6210ab8bcbbab73c47797
Jim Lind <spoon.vw@gmail.com>
2014-12-09 20:20:14 -0600

    No muxing needed.
    AACGain works on M4V files without any problem.
    Bonus.

--- src/TiVampyre/Video/Clean.php
+++ src/TiVampyre/Video/Clean.php
@@ -23,20 +23,7 @@ class Clean
     public function clean($inputFileList, $outputFile)
     {
         $this->merge($inputFileList, $outputFile);
-
-        $videoTrack = $this->demux($outputFile, 1);
-        $audioTrack = $this->demux($outputFile, 2);
-        unlink($outputFile);
-
-        $videoFile = $videoTrack . '.h264';
-        $audioFile = $audioTrack . '.aac';
-
-        rename($videoTrack, $videoFile);
-        rename($audioTrack, $audioFile);
-
-        $this->adjustAudio($audioFile);
-
-        $this->mux($videoFile, $audioFile, $outputFile);
+        $this->adjustAudio($outputFile);
     }
 
     protected function merge($inputFileList, $outputFile)
@@ -55,31 +42,9 @@ class Clean
         }
     }
 
-    protected function demux($inputFile, $track)
+    protected function adjustAudio($m4vFile)
     {
-        $command = 'MP4Box -raw ' . $track . ' ' . $inputFile;
-        $this->process->setCommandLine($command);
-        $this->process->setTimeout(0); // No timeout.
-        $this->process->run();
-
-        $fileRoot = substr($inputFile, 0, strrpos($inputFile, '.'));
-        return $fileRoot . '_track' . $track;
-    }
-
-    protected function mux($videoFile, $audioFile, $outputFile)
-    {
-        $command = 'MP4Box -new ' . $outputFile . ' -add ' . $videoFile . ' -add ' . $audioFile;
-        $this->process->setCommandLine($command);
-        $this->process->setTimeout(0); // No timeout.
-        $this->process->run();
-
-        unlink($videoFile);
-        unlink($audioFile);
-    }
-
-    protected function adjustAudio($aacFile)
-    {
-        $command = 'aacgain -f -r -c ' . $aacFile;
+        $command = 'aacgain -f -r -c ' . $m4vFile;
         $this->process->setCommandLine($command);
         $this->process->setTimeout(0); // No timeout.
         $this->process->run();
