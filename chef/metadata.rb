20738b8c09367c6aec61cfa7521776432a80475c
Jim Lind <spoon.vw@gmail.com>
2015-01-08 18:47:04 -0600

    Config Whitespace

--- chef/metadata.rb
+++ chef/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 version          '0.1.0'
 
-depends 'apt', '~> 2.6.0'
+depends 'apt',  '~> 2.6.0'
 depends 'hhvm', '~> 0.6.0'
 
 depends 'tivo', '~> 0.0.1'
