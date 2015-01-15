2eab8a12a5313e5a92aa9edb0575a666207b4b8b
Jim Lind <spoon.vw@gmail.com>
2015-01-14 22:34:03 -0600

    Trim pieces now that beanstalkd is used.
    I don’t have to have a home brew job queue.
    I do want to have an idea what is in the beanstalkd tube though.

--- TiVampyre/src/console.php
+++ TiVampyre/src/console.php
@@ -28,50 +28,31 @@ $console->register('db-setup')
                     ts             TEXT
                 )';
             $app['db']->query($showSQL);
-            $jobQueueSQL = '
-                CREATE TABLE job_queue (
+            $tubeSQL = '
+                CREATE TABLE tube_log (
                     id      INTEGER PRIMARY KEY AUTOINCREMENT,
                     show_id INTEGER,
-                    status  INTEGER,
-                    ts      TEXT
+                    tube    TEXT
                 )';
-            $app['db']->query($jobQueueSQL);
-            $jobStatusSQL = '
-                CREATE TABLE job_status (
-                        id   INTEGER PRIMARY KEY,
-                        name TEXT
-                );
-                INSERT INTO job_status(id, name) VALUES (1,"QUEUED");
-                INSERT INTO job_status(id, name) VALUES (2,"DOWNLOADING");
-                INSERT INTO job_status(id, name) VALUES (3,"DOWNLOADED");
-                INSERT INTO job_status(id, name) VALUES (4,"DOWNLOADED");
-                INSERT INTO job_status(id, name) VALUES (5,"ENCODING");
-                INSERT INTO job_status(id, name) VALUES (6,"COMPLETE");
-                INSERT INTO job_status(id, name) VALUES (13,"ERROR");
-            ';
-            $app['db']->query($jobStatusSQL);
+            $app['db']->query($tubeSQL);
         });
 
 $console->register('db-destroy')
         ->setDescription('Destroy the SQLite database tables.')
         ->setCode(function() use ($app) {
-            $dropShow = 'DROP TABLE show';
-            $app['db']->query($dropShow);
-            $dropQueue = 'DROP TABLE job_queue';
-            $app['db']->query($dropQueue);
-            $dropStatus = 'DROP TABLE job_status';
-            $app['db']->query($dropStatus);
+            $showSQL = 'DROP TABLE show';
+            $app['db']->query($showSQL);
+            $tubeSQL = 'DROP TABLE tube_log';
+            $app['db']->query($tubeSQL);
         });
 
 $console->register('db-truncate')
         ->setDescription('Truncate the SQLite database tables')
         ->setCode(function() use ($app) {
-            $truncateShow = 'DELETE FROM show';
-            $app['db']->query($truncateShow);
-            $truncateQueue = 'DELETE FROM job_queue';
-            $app['db']->query($truncateQueue);
-            $truncateStatus = 'DELETE FROM job_status';
-            $app['db']->query($truncateStatus);
+            $showSQL = 'DELETE FROM show';
+            $app['db']->query($showSQL);
+            $tubeSQL = 'DELETE FROM tube_log';
+            $app['db']->query($tubeSQL);
         });
 
 $console->register('get-shows')
@@ -102,80 +83,6 @@ $console->register('list-shows')
             }
         });
 
-$console->register('download')
-        ->setDefinition(
-            array(
-                new InputArgument('Show Id', InputArgument::REQUIRED, 'The unique TiVo Id for the show.'),
-                new InputOption('skip', 's', InputOption::VALUE_NONE, 'Skip video transcoding, keep MPEG file.'),
-                new InputOption('keep', 'k', InputOption::VALUE_NONE, 'Keep the original MPEG file after transcoding.'),
-                new InputOption('auto', 'a', InputOption::VALUE_NONE, 'Autocrop black borders.'),
-                new InputOption('cut', 'c', InputOption::VALUE_NONE, 'Cut commericials from file while transcoding.'),
-                new InputOption('dvd', 'd', InputOption::VALUE_NONE, 'Transcode to NTSC DVD file (NOT IMPLMENTED).'),
-            )
-        )
-        ->setDescription('Download and convert a show.')
-        ->setCode(function(InputInterface $input, OutputInterface $output) use ($app){
-            $showId     = $input->getArgument('Show Id');
-            $optionList = $input->getOptions();
-
-            $repository = $app['orm.em']->getRepository('TiVampyre\Entity\Show');
-            $showEntity = $repository->find($showId);
-            if (!$showEntity) {
-                $output->write('Show Not Found', true);
-                return;
-            }
-
-            $rawFilename = $app['tivampyre_working_directory'] . $showEntity->getId();
-
-            $output->write('Downloading...', true);
-            $app['tivo_downloader']->store(
-                $showEntity->getURL(),
-                $rawFilename . '.tivo'
-            );
-
-            $output->write('Decoding...', true);
-            $app['tivo_decoder']->decode(
-                $rawFilename . '.tivo',
-                $rawFilename . '.mpeg'
-            );
-            unlink($rawFilename . '.tivo');
-
-            if ($optionList['skip']) {
-                $output->write('Downloaded to ' . $rawFilename . '.mpeg', true);
-                return;
-            }
-
-            $chapterList = array();
-            if ($optionList['cut']) {
-                $output->write('Looking for Commercials...', true);
-                $chapterList = $app['comskip']->getChapterList($rawFilename . '.mpeg');
-            }
-
-            $output->write('Transcoding...', true);
-            $fileList = $app['video_transcoder']->transcode(
-                $rawFilename . '.mpeg',
-                $chapterList,
-                $optionList['auto']
-            );
-
-            $output->write('Cleaning MP4...', true);
-            $app['video_cleaner']->clean(
-                $fileList,
-                $rawFilename . '.m4v'
-            );
-
-            $app['video_labeler']->addMetadata($showEntity, $rawFilename . '.m4v');
-            $cleanFilename = $app['video_labeler']->renameFile($showEntity, $rawFilename . '.m4v');
-
-            $output->write('Downloaded to ' . $cleanFilename, true);
-
-            if ($optionList['keep']) {
-                $output->write('Original MPEG written to ' . $rawFilename . '.mpeg', true);
-            } else {
-                unlink($rawFilename . '.mpeg');
-            }
-        });
-
 $console->register('queue')
         ->setDefinition(
             array(
@@ -195,6 +102,23 @@ $console->register('queue')
 
             $app['queue']->useTube('download')
                          ->put(json_encode($optionList));
+        });
+
+$console->register('queue-status')
+        ->setCode(function(InputInterface $input, OutputInterface $output) use ($app){
+            $pheanstalk = $app['queue'];
+            $listening  = $pheanstalk->getConnection()->isServiceListening();
+            if ($listening) {
+                $output->write('Beanstalkd is listening.', true);
+            } else {
+                $output->write('Beanstalkd is NOT listening.', true);
+            }
+
+            $tubeList = $pheanstalk->listTubes();
+            foreach($tubeList as $tube) {
+                $tubeStats = $pheanstalk->statsTube($tube);
+                $output->write($tubeStats['current-jobs-ready'] . ' items in ' . $tube . ' tube.', true);
+            }
         });
 
 $console->register('download-worker')
