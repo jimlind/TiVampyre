1848f2be4e0e8af79722c277f0c2fc48b9ce663f
Jim Lind <jimlind@me.com>
2015-05-23 17:41:50 -0500

    Rename Show to ShowRepository
    To avoid name conflicts.
    Also removed unused methods, so it doesn’t look like a straight rename.

---
+++ TiVampyre/src/TiVampyre/Repository/ShowRepository.hh
@@ -0,0 +1,59 @@
+<?hh
+
+namespace TiVampyre\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Repository for Show Entity.
+ */
+class ShowRepository extends EntityRepository
+{
+    /**
+     * Returns a list of all shows entities roughly sorted.
+     *
+     * @return TiVampyre\Entity\ShowEntity[]
+     */
+    public function getAllSortedEpisodes(): array
+    {
+        $queryBuilder = $this->createQueryBuilder('s');
+        $queryBuilder->addOrderBy('s.showTitle', 'ASC');
+        $queryBuilder->addOrderBy('s.episodeNumber', 'ASC');
+        $queryBuilder->addOrderBy('s.date', 'ASC');
+
+        $query = $queryBuilder->getQuery();
+
+        return $query->getResult();
+    }
+
+    /**
+     * Returns a list of all outdated shows entities.
+     *
+     * @return TiVampyre\Entity\ShowEntity[]
+     */
+    public function findOutdated(): array
+    {
+        $queryBuilder = $this->createQueryBuilder('s');
+        $queryBuilder->where('s.ts < :timeStamp');
+        $queryBuilder->setParameter('timeStamp', $this->findMaxTimeStamp());
+
+        $query = $queryBuilder->getQuery();
+
+        return $query->getResult();
+    }
+
+    /**
+     * Returns the most recent show entity timestamp.
+     *
+     * @return string
+     */
+    public function findMaxTimeStamp(): string
+    {
+        $queryBuilder = $this->createQueryBuilder('s');
+        $queryBuilder->select('MAX(s.ts)');
+
+        $query = $queryBuilder->getQuery();
+
+        return $query->getSingleScalarResult();
+    }
+}
