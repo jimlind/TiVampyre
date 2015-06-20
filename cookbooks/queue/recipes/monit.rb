bfd5eda15d12b9cd20b90fe6f8eb4da937d93a15
Jim Lind <jimlind@me.com>
2015-06-20 18:58:10 -0500

    Write Monit Configuration

--- cookbooks/queue/recipes/monit.rb
+++ cookbooks/queue/recipes/monit.rb
@@ -2,3 +2,14 @@ apt_package 'monit' do
   options '--no-install-recommends'
   action  :install
 end
+
+cookbook_file '/etc/monit/monitrc' do
+   owner 'root'
+   group 'root'
+   mode '0600'
+   source 'monitrc'
+end
+
+service 'monit' do
+  action :restart
+end
