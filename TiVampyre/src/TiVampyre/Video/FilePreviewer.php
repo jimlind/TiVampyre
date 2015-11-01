4f20c589eced8259aca9f12d22a4ffe11dcfe721
Jim Lind <spoon.vw@gmail.com>
2015-11-01 12:58:22 -0600

    Run all the files through handbrake first.
    avconv isn't as reliable as handbrake

--- TiVampyre/src/TiVampyre/Video/FilePreviewer.php
+++ TiVampyre/src/TiVampyre/Video/FilePreviewer.php
@@ -35,26 +35,12 @@ class FilePreviewer
 
     public function preview($filePath): string
     {
-        $arguments = [
-            '-i',
-            $filePath,
-            '-deinterlace',
-            '-q',
-            1,
-            '-r',
-            1,
-            '-f',
-            'image2',
-            "{$filePath}.%04d.jpg",
-        ];
-
-        $this->processBuilder->setPrefix('avconv');
-        $this->processBuilder->setArguments($arguments);
-        $this->processBuilder->setTimeout(0);
-        $this->processBuilder->getProcess()->run();
+        $mp4File = $this->handbrakeRunner($filePath);
+        $this->avconvRunner($mp4File);
+        unlink($mp4File);
 
         $previewName = "{$filePath}.jpg";
-        $previewList = glob("{$filePath}.*.jpg");
+        $previewList = glob("{$mp4File}.*.jpg");
         $lastItem    = array_pop($previewList);
         rename($lastItem, $previewName);
 
@@ -63,5 +49,44 @@ class FilePreviewer
         }
 
         return $previewName;
+    }
+
+    protected function handbrakeRunner($filePath) {
+        $outputFile = "{$filePath}.tmp.mp4";
+        $arguments  = [
+            '--input=' . $filePath,
+            '--quality=1',
+            '--rate=5',
+            '--decomb',
+            '--audio=none',
+            '--out=' . $outputFile,
+        ];
+
+        $this->processBuilder->setPrefix('HandBrakeCLI');
+        $this->processBuilder->setArguments($arguments);
+        $this->processBuilder->setTimeout(0);
+        $this->processBuilder->getProcess()->run();
+
+        return $outputFile;
+    }
+
+    private function avconvRunner($filePath) {
+        $outputFile = "{$filePath}.%04d.jpg";
+        $arguments  = [
+            '-i',
+            $filePath,
+            '-q',
+            1,
+            '-r',
+            1,
+            '-f',
+            'image2',
+            $outputFile,
+        ];
+
+        $this->processBuilder->setPrefix('avconv');
+        $this->processBuilder->setArguments($arguments);
+        $this->processBuilder->setTimeout(0);
+        $this->processBuilder->getProcess()->run();
     }
 }
